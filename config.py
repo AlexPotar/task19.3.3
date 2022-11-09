61e16bdedd2e0564ca2699f17eb9999a72daa16e
AlexPotar <109272592+AlexPotar@users.noreply.github.com>
2022-11-09 13:43:32 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,85 @@
+# SkillFactory QAP-89 задание 19.3.3
+
+# Имя пользователя для логина
+username = 'Alexx'
+
+# Пароль пользователя для логина
+password = 'quant'
+
+# Данные для создания нового юзера
+created_user = {
+  "id": 0,
+  "username": "alexx22",
+  "firstName": "AL",
+  "lastName": "Popov",
+  "email": "alpopov3@ttt.org",
+  "password": "Quant123456",
+  "phone": "+12345678901",
+  "userStatus": 0
+}
+
+# Данные для обновления юзера
+updated_user = {
+  "id": 0,
+  "username": "alexx223",
+  "firstName": "AL2",
+  "lastName": "Popova",
+  "email": "alpopov333@ttt.org",
+  "password": "Quant12345678",
+  "phone": "+12345678922",
+  "userStatus": 0
+}
+
+# Данные для создания пользователей списком
+list_of_users = [
+  {
+    "id": 0,
+    "username": "Bob",
+    "firstName": "Roger",
+    "lastName": "Svan",
+    "email": "svansvan@rtr.org",
+    "password": "svanddd",
+    "phone": "+45454577771",
+    "userStatus": 0
+  },
+  {
+    "id": 0,
+    "username": "rob",
+    "firstName": "Gold",
+    "lastName": "Goldman",
+    "email": "gold@rtts.org",
+    "password": "rob2255",
+    "phone": "+76848382198",
+    "userStatus": 0
+  }
+]
+
+# Данные для создания ордера
+order = {
+  "id": 0,
+  "petId": 0,
+  "quantity": 0,
+  "shipDate": "0",
+  "status": "placed",
+  "complete": True
+}
+
+# Данные для добавления нового питомца
+new_pet = {
+  "id": 0,
+  "category": {
+    "id": 0,
+    "name": "Category"
+  },
+  "name": "Name",
+  "photoUrls": [
+    "None"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "Tags"
+    }
+  ],
+  "status": "available"
+}
